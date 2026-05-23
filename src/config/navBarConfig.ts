79b4c4797751c8251cf7b2c5cfa73022587b9c9f
prnmjpngst <prnmjpngst@gmail.com>
2026-05-23 09:35:31 +0700

    fix(nav): correct syntax error in navBarConfig

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -16,9 +16,10 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 
 		// 归档
 		LinkPreset.Archive,
-		];
+	];
 
-		// 根据配置决定是否添加留言板，在siteConfig关闭pages.guestbook时导航栏不显示留言板	if (siteConfig.pages.guestbook) {
+	// 根据配置决定是否添加留言板，在siteConfig关闭pages.guestbook时导航栏不显示留言板
+	if (siteConfig.pages.guestbook) {
 		links.push(LinkPreset.Guestbook);
 	}
 
